0212be84c08fb4581b22c02f094b19f4563572b7
Ari Reza Prakasa <165811889+arirezap@users.noreply.github.com>
2026-08-11 16:35:15 +0700

    perbaikan tampilan lapor harian

--- app/Models/LogKegiatanHarian.php
+++ app/Models/LogKegiatanHarian.php
@@ -32,7 +32,7 @@ class LogKegiatanHarian extends Model
     public function getLogWithTarget($userId, $tanggal)
     {
         return $this->select('log_kegiatan_harian.*, laporan_harian.sasaran_program, laporan_harian.indikator_kinerja, laporan_harian.satuan')
-                    ->join('laporan_harian', 'laporan_harian.id = log_kegiatan_harian.target_id')
+                    ->join('laporan_harian', 'laporan_harian.id = log_kegiatan_harian.target_id', 'left')
                     ->where('log_kegiatan_harian.user_id', $userId)
                     ->where('log_kegiatan_harian.tanggal_kegiatan', $tanggal)
                     ->orderBy('log_kegiatan_harian.created_at', 'ASC')
@@ -42,7 +42,7 @@ class LogKegiatanHarian extends Model
     public function getLogByMonth($userId, $bulan, $tahun, $onlySubmitted = false)
     {
         $builder = $this->select('log_kegiatan_harian.*, laporan_harian.sasaran_program, laporan_harian.indikator_kinerja, laporan_harian.target_bulanan, laporan_harian.satuan')
-                    ->join('laporan_harian', 'laporan_harian.id = log_kegiatan_harian.target_id')
+                    ->join('laporan_harian', 'laporan_harian.id = log_kegiatan_harian.target_id', 'left')
                     ->where('log_kegiatan_harian.user_id', $userId)
                     ->where('MONTH(log_kegiatan_harian.tanggal_kegiatan)', $bulan)
                     ->where('YEAR(log_kegiatan_harian.tanggal_kegiatan)', $tahun);
